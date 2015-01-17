1eff199fd7c3aa147186d3d4b8caaaabb26b1e5b
AndyLin <lmz8241@nisra.net>
2015-01-18 03:20:32 +0800

    Add Group implements

--- server/mobile/views.py
+++ server/mobile/views.py
@@ -9,12 +9,14 @@ from mobile.models import User
 from mobile.models import Group
 from mobile.models import Choose
 from mobile.models import Vote
+from mobile.models import Day
 from mobile.models import Join
 import json
 import random
 STATUSCODE = {
     "ADDUSERSUCCESS":"201",
     "VOTESUCCESS":"202",
+    "ADDGROUPSUCCESS":"203",
     "PARAMETERMISS":"301",
     "TOOLONGPARAMETER":"302",
     "SQLERROR":"303",
@@ -111,7 +113,38 @@ def addGroup(request):
                          groupPushTime=groupPushTime,
                          owner_id=owner_id,
                          defaultValue=defaultValue)
-        newGroup.gruopID
+        newGroup.groupID
+        newGroup.save()
+        newGroup = Group.objects.get(groupID=newGroup.groupID)
+        Mon = request.GET["daysetmon"]
+        Tue = request.GET["daysettue"]
+        Wed = request.GET["daysetwed"]
+        Thu = request.GET["daysetthu"]
+        Fri = request.GET["daysetfri"]
+        Sat = request.GET["daysetsat"]
+        Sun = request.GET["daysetsun"]
+        newDay = Day(Mon=Mon,Tue=Tue,Wed=Wed,Thu=Thu,Fri=Fri,Sat=Sat,Sun=Sun,groupID=newGroup)
+        newDay.save()
+        newJoin = Join(groupID=newGroup,userID=owner_id)
+        newJoin.save()
+        groupList = []
+        for ids in Join.objects.filter(userID=owner_id).values_list('groupID','isJoin'):
+            groupID = ids[0]
+            isJoin = ids[1]
+            groupName = Group.objects.get(groupID=groupID).groupName
+            result = {"groupID":groupID,
+                      "groupName":groupName,
+                      "isJoin":isJoin}
+            groupList.append(result)
+        response["groupList"] = groupList
+        response["status"] = STATUSCODE["ADDGROUPSUCCESS"]
+        data = '%s(%s);' % (callback,json.dumps(response))
+        return HttpResponse(data,content_type="application/javascript")
+
+
+
+
+
 
 
 
