ac6cb50d62f15e758bf392c1823e059989bc9545
AndyLin <lmz8241@nisra.net>
2015-01-18 02:48:07 -0500

    vote

--- server/mobile/views.py
+++ server/mobile/views.py
@@ -75,6 +75,7 @@ def viewUserName(request):
     return HttpResponse(json.dumps(response),content_type="application/json")
 def vote(request):
     response = {}
+    callback = "vote"
     if not ("userID" in request.POST and "chooseID" in request.POST):
         response["status"] = STATUSCODE["PARAMETERMISS"]
     else:
@@ -93,7 +94,8 @@ def vote(request):
                 response["status"] = (STATUSCODE["VOTESUCCESS"])
             else:
                 response["status"] = (STATUSCODE[""])
-    return HttpResponse(json.dumps(response),content_type="application/json")
+    data = '%s(%s);' % (callback,json.dumps(response))
+    return HttpResponse(data,content_type="application/json")
 def viewGroup(request):
     response = {}
     callback = 'view_group_callback'
@@ -430,12 +432,12 @@ def checkPush(request):
     else:
         userID = request.GET["userID"]
         searchResult = []
-        for userInGroup in Join.objects.filter(userID_id=userID).value_list('groupID_id',flat=True):
-            for nowTimePushGroup in Group.filter(Q(groupID=userInGroup) & Q(groupPushTime=strftime("%H:%M"))).value_list('groupID',flat=True):
+        for userInGroup in Join.objects.filter(userID_id=userID).values_list('groupID_id',flat=True):
+            for nowTimePushGroup in Group.objects.filter(Q(groupID=userInGroup) & Q(groupPushTime=strftime("%H:%M"))).values_list('groupID',flat=True):
                 if isTodayPush(nowTimePushGroup):
                     maxVoteNumber = -1
                     maxChoose = -1
-                    for choose in Choose.objects.filter(group_id=nowTimePushGroup).value_list('chooseID',flat=True):
+                    for choose in Choose.objects.filter(group_id=nowTimePushGroup).values_list('chooseID',flat=True):
                         voteNumber = Vote.objects.filter(chooseID_id=choose).count()
                         if (voteNumber > 0 and voteNumber > maxVoteNumber):
                             maxVoteNumber = voteNumber
@@ -447,7 +449,7 @@ def checkPush(request):
                     else:
                         pushGroup.today_Value = Choose.objects.get(chooseID=maxChoose).chooseName
                         pushGroup.save()
-                    members_id = Join.objects.filter(groupID=pushGroup).value_list('userID_id',flat=True)
+                    members_id = Join.objects.filter(groupID=pushGroup).values_list('userID_id',flat=True)
                     members = []
                     for member_id in members_id:
                         memberName = User.objects.get(userID=member_id).userName
